fa001355954da7a6786c161caf94c14562c88c2b
henri Joel SEDJAME <sedhjo@gmail.com>
2018-11-29 16:37:35 +0100

    Notification-server : ajout du projet

---
+++ notification-server/src/main/java/org/myaldoc/notificationserver/NotificationServerApplication.java
@@ -0,0 +1,14 @@
+package org.myaldoc.notificationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NotificationServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NotificationServerApplication.class, args);
+  }
+}
